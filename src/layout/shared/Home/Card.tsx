b3b20e3f87734b2f09da381d8e8bff8be15ef448
vinitran124 <vinh.tran@icetea.io>
2024-02-27 13:31:07 +0700

    feat: update card match

--- src/layout/shared/Home/Card.tsx
+++ src/layout/shared/Home/Card.tsx
@@ -5,7 +5,7 @@ import styled from "styled-components";
 const MatchBlock = styled.div`
     background-color: #7a7d82;
     padding: 15px 10px;
-    width: 370px;
+    width: 430px;
     margin: 20px 20px;
     border-radius: 10px;
     align-items: center;
@@ -34,12 +34,32 @@ const WrapperTeam = styled.div`
     justify-content: center;
 `;
 
+const WrapperTime = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 0px 0px;
+`;
+
+const WrapperScore = styled.div`
+    display: flex;
+    min-width:50px;
+    font-size: 20px;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0px 0px;
+`;
+
+
 interface MatchCardProps {
     data: MatchModule.Matchs;
 }
 
 export const MatchCard: React.FC<MatchCardProps> = ({ data }) => {
+    const time = new Date(data.timestamp);
     return (
+        data.is_live? 
         <Link href={data.id} as={`/watch/${data.id}`}>
             <MatchBlock>
                 <WrapperTeam>
@@ -50,6 +70,15 @@ export const MatchCard: React.FC<MatchCardProps> = ({ data }) => {
                     </LogoWrapper>
                     <div>{data.home.name}</div>
                 </WrapperTeam>
+                <WrapperScore>
+                    <div>
+                        {data.scores.home}
+                    </div>
+                    :
+                    <div>
+                        {data.scores.away}
+                    </div>
+                </WrapperScore>
                 <WrapperTeam>
                     <LogoWrapper>
                         <Logo>
@@ -60,5 +89,32 @@ export const MatchCard: React.FC<MatchCardProps> = ({ data }) => {
                 </WrapperTeam>
             </MatchBlock>
         </Link>
+        :
+        <MatchBlock>
+                <WrapperTeam>
+                    <LogoWrapper>
+                        <Logo>
+                            <img src={data.home.logo} />
+                        </Logo>
+                    </LogoWrapper>
+                    <div>{data.home.name}</div>
+                </WrapperTeam>
+                <WrapperTime>
+                    <div>
+                        {`${data.date.substring(6,8)}/${data.date.substring(4,6)}/${data.date.substring(0,4)}`}
+                    </div>
+                    <div>
+                        {`${time.getHours()}:${time.getMinutes().toString().padStart(2, '0')}`}
+                    </div>
+                </WrapperTime>
+                <WrapperTeam>
+                    <LogoWrapper>
+                        <Logo>
+                            <img src={data.away.logo} />
+                        </Logo>
+                    </LogoWrapper>
+                    <div>{data.away.name}</div>
+                </WrapperTeam>
+            </MatchBlock>
     );
 };
